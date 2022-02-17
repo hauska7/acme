ba6c3be7cfadf5a6b008a91ac8a7072facfe1a40
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2022-02-17 11:03:11 +0100

    set next charge date service tests

--- spec/models/signup_spec.rb
+++ spec/models/signup_spec.rb
@@ -16,37 +16,4 @@ RSpec.describe Signup do
       expect(signup.save).to be true
     end
   end
-
-  describe '.set_next_charge_date' do
-    context 'when next_charge_date is nil' do
-      it 'sets next charge date' do
-        signup = create!(:signup, next_charge_date: nil)
-
-        signup.set_next_charge_date
-
-        expect(signup.next_charge_date).to be_present
-      end
-    end
-
-    context 'when next_charge_date is in the future' do
-      it 'does not change next charge date' do
-        signup = create!(:signup, next_charge_date: Date.tomorrow)
-
-        expect do
-          signup.set_next_charge_date
-        end.not_to(change { signup.next_charge_date })
-      end
-    end
-
-    context 'when next_charge_date is not in the future' do
-      it 'sets next charge date in the furure' do
-        old_next_charge_date = Date.today
-        signup = create!(:signup, next_charge_date: old_next_charge_date)
-
-        signup.set_next_charge_date
-
-        expect(signup.next_charge_date > old_next_charge_date).to be true
-      end
-    end
-  end
 end
---
+++ spec/services/set_next_charge_date_service_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SetNextChargeDateService do
+  describe '#set_next_charge_date' do
+    context 'when next_charge_date is nil' do
+      it 'sets next charge date' do
+        signup = create!(:signup, next_charge_date: nil)
+
+        described_class.call(signup)
+
+        expect(signup.next_charge_date).to be_present
+      end
+    end
+
+    context 'when next_charge_date is in the future' do
+      it 'does not change next charge date' do
+        signup = create!(:signup, next_charge_date: Date.tomorrow)
+
+        expect do
+          described_class.call(signup)
+        end.not_to(change { signup.next_charge_date })
+      end
+    end
+
+    context 'when next_charge_date is not in the future' do
+      it 'sets next charge date in the furure' do
+        old_next_charge_date = Date.today
+        signup = create!(:signup, next_charge_date: old_next_charge_date)
+
+        signup.set_next_charge_date
+
+        expect(signup.next_charge_date > old_next_charge_date).to be true
+      end
+    end
+  end
+end
